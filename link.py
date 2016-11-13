58519e5e69e06aa2daeb458b2252b428398a3483
Kevin Ye <kevincye1996@gmail.com>
2016-11-12 23:30:52 -0800

    Cleaning up some auto-id nonsense, added ends array to link, modified 'to_string' for links

--- link.py
+++ link.py
@@ -7,8 +7,7 @@ class Link:
     l_map = {}
 
     def __init__(self, link_id, rate, prop_delay, buffer_size):
-        # IDs start from 1.
-        self.id = len(Link.ids) + 1
+        self.id = link_id
         Link.ids.append(self.id)
 
         self.rate = rate       # in bytes per second
@@ -17,6 +16,14 @@ class Link:
 
         self.buffer = []
         self.buf_processing = False
+
+        # Ends is a list that contains the object on either side of the list.
+        # Its size at any time should be at most two.
+        self.ends = []
+
+    def add_end(self, entity):
+        assert(len(self.ends) < 2)
+        self.ends.append(entity)
 
     def buffer_add(self, pkt):
         self.buffer.append(pkt)
@@ -30,7 +37,7 @@ class Link:
     def __str__(self):
         return "<Link ID: " + str(self.id) + ", Link Rate: " + str(self.rate) + \
             ", Propogation Delay: " + str(self.prop_delay) + ", Buffer size: " + \
-            str(self.buffer_size) + ">"
+            str(self.buffer_size) + ", Ends: " + str(self.ends) + "ENDS>\n"
 
     __repr__ = __str__
 
