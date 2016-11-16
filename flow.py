ae2bf0941f1c5fc7172a88a644ec481404b45991
Chen <riichenchen@gmail.com>
2016-11-16 06:45:01 -0800

    Initial implementation of sending windows and flows responding to packets (just ACKs for now)

--- flow.py
+++ flow.py
@@ -1,5 +1,5 @@
 from math import ceil
-from pqueue import event_queue, enqueue
+from pqueue import event_queue, enqueue, get_global_time, qempty
 import packet
 import event
 
@@ -15,6 +15,10 @@ class Flow:
         self.data_amt = data_amt
         self.start_time = start_time
 
+        self.window_size = 100
+        self.curr_pkt = 0
+        self.num_packets = int(ceil(data_amt * 1.0e6 / packet.DataPkt.PACKET_SIZE))
+
 
     def __str__(self):
         return "<Flow ID: " + str(self.id) + ", Source: " + str(self.source) +  \
@@ -29,9 +33,23 @@ class Flow:
         # Where pkt_size is 1024 bytes.
 
         # =====TODO: put PACKET_SIZE as a global variable for reference?====
+        while (self.curr_pkt < min(self.num_packets, self.window_size)):
+            pkt = packet.DataPkt(self.source, self.destination, \
+                "PACKET %d" % self.curr_pkt, self.curr_pkt, self)
+            enqueue(event.SendPacket(self.start_time, pkt, \
+                self.source.link, self.source))
+            self.curr_pkt += 1
 
-        num_packets = int(ceil(self.data_amt * 1.0e6 / packet.Packet.PACKET_SIZE))
-
+        '''
         for i in range(num_packets):
             pkt = packet.Packet(self.source, self.destination, i, i)
             enqueue(event.SendPacket(1, pkt, self.source.link, self.source))
+        '''
+    def receiveAck(self, ack, curr_time):
+        if (self.curr_pkt < self.num_packets):
+            pkt = packet.DataPkt(self.source, self.destination, \
+                "PACKET %d" % self.curr_pkt, self.curr_pkt, self)
+            enqueue(event.SendPacket(curr_time, pkt, self.source.link, self.source))
+            self.curr_pkt += 1
+
+
