50b0a6f3ff99d954ae2d62575b2d0712bdd97271
sning <sning@caltech.edu>
2016-11-26 15:58:42 -0800

    Fixed receiveAck to fit the description in the textbook, i.e. hosts send ACKs where the ack number is the packet it expects next. Currently does dumb shit where, upon a dropped packet, sends that dropped packet like a bajillion times. Put in the comments above receiveAck a way to refactor the code so that it's smarter.

--- flow.py
+++ flow.py
@@ -51,26 +51,44 @@ class Flow:
             pkt = packet.Packet(self.source, self.destination, i, i)
             enqueue(event.SendPacket(1, pkt, self.source.link, self.source))
         '''
+        # Fix receiveAck to be in this format:
+        # 1. if ACK is a duplicate ACK
+        #   - move into fast recovery state
+        #   - window is W/2 + 1
+        #   - set dup_ack_num = ack.number
+        #   - increase window by +1 for every additional dup-ack
+        #   - exit fast recovery state when a non-dup ack is received
+        # 2. if not, update window
+        #   - depends on algorithm: Reno or FAST
+        # 3. (execute regardless of whether 1 or 2 happened) if we have space in our window (i.e. calculate window_size - unacknowledged)
+        #   - send out that many packets
+        #
     def receiveAck(self, ack, curr_time):
-        # Take the packet for which we've received an ACK off the
-        # unacknowledged list.
-        if self.unacknowledged.pop(ack.number, None) == None:
-            # Recieved an ack for something that's been
-            # acknowledged already = duplicate packet.
+        # We recieve ACKs with number = the next packet it expects.
+        # This means packet number (ack.number - 1) was received,
+        # so we delete that packet num from the unacknowledged HT.
+
+        # If that packet has already been acknowledged (i.e. not in
+        # the HT) then we have a duplicate ACK for packet ack.number.
+        if self.unacknowledged.pop(ack.number - 1, None) == None:
             # Remake the missing packet.
-            'resending dropped packet ', ack.number
+            print 'resending dropped packet ', ack.number
             pkt = packet.DataPkt(self.source, self.destination, \
                 "PACKET %d" % ack.number, ack.number, self)
+
             # Resend the packet.
             enqueue(event.SendPacket(curr_time, pkt, self.source.link, \
              self.source))
             #enqueue(event.PacketTimeout(curr_time + self.timeout, pkt))
             # Re-add it to our unacknowledged hash.
             self.unacknowledged[ack.number] = 0
+            
+            # Set the curr_pkt to the next packet that was dropped.
+            self.curr_pkt = ack.number + 1
 
         # If we don't have to resend a packet, we can slide our window
         # over to send new packets.
-        else:    
+        else:   
             if (self.curr_pkt < self.num_packets):
                 pkt = packet.DataPkt(self.source, self.destination, \
                     "PACKET %d" % self.curr_pkt, self.curr_pkt, self)
