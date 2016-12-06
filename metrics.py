4b7f6b8a5b7cd8d32563d0f7e72cc1b473592071
Kevin Ye <kevincye1996@gmail.com>
2016-12-05 21:04:28 -0800

    cleanup metrics, only plot relevant links in plots

--- metrics.py
+++ metrics.py
@@ -58,7 +58,7 @@ def update_flow(flow_id, send_r, rec_r, rtts, w_size, time, update_flow_rate):
     if update_flow_rate:
         dict_insert(flow_id, receive_rate, rec_r)
         dict_insert(flow_id, fr_times, time)
-    #dict_insert(flow_id, receive_rate, rec_r)
+
     dict_insert(flow_id, round_trip_time, rtts)
     dict_insert(flow_id, window_sizes, w_size)
     dict_insert(flow_id, f_times, time)
@@ -69,15 +69,6 @@ def report_metrics(time):
         plot_metrics(False, time)
         last_report_time = time
 
-# def print_metrics(time):
-#     global buffer_load, packet_loss, flow_rate
-#     print ("========================================")
-#     print ("Time: %.8fs" % time)
-
-#     for i in link_ids:
-#         print ("Link %s: [Buffer load: %.2f%%, Packet Loss: %d pkts, Flow rate: %.2f pkts/second]" % (i, buffer_load[i], packet_loss[i], flow_rate[i]))
-    
-#     print ()
 
 def plot_metrics(final, time):
     global buffer_load, packet_loss, flow_rate, fig, l_times, f_times, \
@@ -85,8 +76,8 @@ def plot_metrics(final, time):
 
     for i in link_ids:
 
-        # if get_num(i) not in [0, 1, 2, 5]:
-        #     continue
+        if get_num(i) not in [1, 2, 3]:
+            continue
 
         t = l_times[i]
         clr_str = colors[get_num(i)]
@@ -98,13 +89,13 @@ def plot_metrics(final, time):
         ax_fr.plot(lr_times[i], flow_rate[i], color=clr_str, label=i)
 
         ax_bl = fig.add_subplot(612)
-        ax_bl.set_ylim((-1, 100))
+        ax_bl.set_ylim((-1, 140))
         ax_bl.set_xlabel('time (s)')
         ax_bl.set_ylabel('buffer load\n(pkts)')
-        ax_bl.plot(t, buffer_load[i], color=clr_str, label=i, lw=0.4)
+        ax_bl.plot(t, buffer_load[i], color=clr_str, label=i, lw=0.3)
 
         ax_pl = fig.add_subplot(613)
-        #ax_pl.set_ylim((-1, 100))
+        ax_pl.set_ylim((-1, 10))
         ax_pl.set_xlabel('time (s)')
         ax_pl.set_ylabel('packet loss\n(pkts)')
         ax_pl.plot(t, packet_loss[i], color=clr_str, label=i)
@@ -117,33 +108,22 @@ def plot_metrics(final, time):
         clr_str = colors[get_num(i)]
 
         ax_sr = fig.add_subplot(614)
-        #ax_sr.plot(t, send_rate[i], color=clr_str, label=i)
-        #ax_sr.plot(t, receive_rate[i], color='green', label=i)
+
         ax_sr.set_xlabel('time (s)')
         ax_sr.set_ylabel('flow rate\n(Mbps)')
         ax_sr.plot(fr_times[i], receive_rate[i], color=clr_str, label=i)
 
         ax_ws = fig.add_subplot(615)
-        ax_ws.plot(t, window_sizes[i], color=clr_str, label=i, lw=0.2)
+        ax_ws.plot(t, window_sizes[i], color=clr_str, label=i, lw=0.3)
         ax_ws.set_xlabel('time (s)')
         ax_ws.set_ylabel('window size\n(pkts)')
 
-        # avg_ws = []
-        # sum_ = 0
-        # for j in xrange(len(window_sizes[i])):
-        #     avg_ws.append((sum_ + window_sizes[i][j]) / t[j])
-
-        # ax_ws.plot(t, avg_ws, color=avg_color, label=i)
-
         ax_rtt = fig.add_subplot(616)
-        ax_rtt.plot(t, round_trip_time[i], color=clr_str, label=i, lw=0.2)
+        ax_rtt.plot(t, round_trip_time[i], color=clr_str, label=i, lw=0.3)
         ax_rtt.set_xlabel('time (s)')
         ax_rtt.set_ylabel('round trip time')
 
         plt.legend(loc='lower right', prop={'size': 9})
-
-    # TODO? Per host metrics. But in our test cases, none of the flows share
-    # a common host for a source or destination
 
 
     if final is False:
